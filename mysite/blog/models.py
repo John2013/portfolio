66c7140556f5eb2db043e680f12c8e04d0ef167a
Evgeny Timofeyev <nawasington@ya.ru>
2019-01-29 11:33:18 +0300

    added slug to models

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,23 +2,33 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from slugify import slugify
 
 
 class Tag(models.Model):
-	name = models.CharField('Тэг', max_length=255, null=False)
+	name = models.CharField('Тэг', max_length=255, null=False, unique=True)
+	slug = models.CharField(max_length=255, null=False, unique=True)
 
 	def __str__(self):
 		return "<tag:{}>".format(self.name)
+
+	def save(self, *args, **kwargs):
+		if self._state.adding:
+			if self.slug is None:
+				self.slug = slugify(self.name)
+
+		super().save(*args, **kwargs)
 
 
 class Article(models.Model):
 	title = models.CharField(
 		'Заголовок', max_length=255, unique=True, null=False
 	)
+	slug = models.CharField(max_length=255, unique=True, null=False)
 	body = models.TextField('Статья', null=False)
 	created_at = models.DateTimeField('Создана')
 	pub_date = models.DateTimeField('Дата публикации')
-	tags = models.ManyToManyField(Tag)
+	tags = models.ManyToManyField(Tag, )
 
 	def __str__(self):
 		return "<article:{}>".format(self.title[:30])
@@ -31,3 +41,17 @@ class Article(models.Model):
 	was_published_recently.admin_order_field = 'pub_date'
 	was_published_recently.boolean = True
 	was_published_recently.short_description = 'Published recently?'
+
+	def save(self, *args, **kwargs):
+
+		if self._state.adding:
+			if self.created_at is None:
+				self.created_at = timezone.now()
+
+			if self.pub_date is None:
+				self.pub_date = timezone.now()
+
+			if self.slug is None:
+				self.slug = slugify(self.title)
+
+		super().save(*args, **kwargs)
