54643c6db8a3df93e875c92a4028795afd30e2bc
Evgeny Timofeyev <nawasington@ya.ru>
2019-02-01 19:55:07 +0300

    articles view and articles by tag

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,3 +5,9 @@ from .models import Article
 class IndexView(generic.ListView):
 	model = Article
 	paginate_by = 10
+	ordering = ['-pk']
+
+
+class TagView(IndexView):
+	def get_queryset(self):
+		return Article.objects.filter(tags__slug__contains=self.kwargs['tag'])
