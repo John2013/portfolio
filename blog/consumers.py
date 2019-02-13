90aff00facea4f19c7c45f0088fc56a8035cc1f6
Evgeny Timofeyev <nawasington@ya.ru>
2019-02-13 16:32:28 +0300

    save comment on its add

--- blog/consumers.py
+++ blog/consumers.py
@@ -38,7 +38,7 @@ class ChatConsumer(WebsocketConsumer):
 		article_pk = text_data_json['articlePk']
 
 		comment = Comment(
-			article=Article.objects.get(article_pk),
+			article=Article.objects.get(pk=article_pk),
 			body=message,
 			nickname=nickname,
 			datetime=datetime
