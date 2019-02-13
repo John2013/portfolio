815bf790f90c603fc789c75f01d0e9cb3c799376
Evgeny Timofeyev <nawasington@ya.ru>
2019-02-13 18:43:02 +0300

    cancel of trying to async consumer

--- blog/consumers.py
+++ blog/consumers.py
@@ -1,34 +1,36 @@
-from channels.generic.websocket import AsyncWebsocketConsumer
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import AsyncWebsocketConsumer, \
+	WebsocketConsumer
 import json
 
-from blog.models import Comment, Article
+from blog.models import Comment
 
 
-class ChatConsumer(AsyncWebsocketConsumer):
+class ChatConsumer(WebsocketConsumer):
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
 		self.room_name = self.scope['url_route']['kwargs']['room_name']
 		self.room_group_name = 'chat_%s' % self.room_name
 
-	async def connect(self):
+	def connect(self):
 		# Join room group
-		await self.channel_layer.group_add(
+		async_to_sync(self.channel_layer.group_add)(
 			self.room_group_name,
 			self.channel_name
 		)
 
-		await self.accept()
+		self.accept()
 
-	async def disconnect(self, close_code):
+	def disconnect(self, close_code):
 		# Leave room group
-		await self.channel_layer.group_discard(
+		async_to_sync(self.channel_layer.group_discard)(
 			self.room_group_name,
 			self.channel_name
 		)
 
 	# Receive message from WebSocket
 	# noinspection PyMethodOverriding
-	async def receive(self, text_data):
+	def receive(self, text_data):
 		text_data_json = json.loads(text_data, encoding='utf8')
 		message = text_data_json['message']
 		nickname = text_data_json['nickname']
@@ -43,7 +45,61 @@ class ChatConsumer(AsyncWebsocketConsumer):
 		)
 		comment.save()
 
-		print('before room_group_send')
+		# Send message to room group
+		async_to_sync(self.channel_layer.group_send)(
+			self.room_group_name,
+			{
+				'type': 'chat_message',
+				'message': message,
+				'nickname': nickname,
+				'datetime': datetime,
+			}
+		)
+
+	# Receive message from room group
+	def chat_message(self, event):
+		message = event['message']
+		nickname = event['nickname']
+		datetime = event['datetime']
+
+		# Send message to WebSocket
+		self.send(text_data=json.dumps({
+			'message': message,
+			'nickname': nickname,
+			'datetime': datetime,
+		}))
+
+
+class ChatAsyncConsumer(AsyncWebsocketConsumer):
+	async def connect(self):
+		print('connect')
+		self.room_name = self.scope['url_route']['kwargs']['room_name']
+		self.room_group_name = 'chat_%s' % self.room_name
+		# Join room group
+		await self.channel_layer.group_add(
+			self.room_group_name,
+			self.channel_name
+		)
+
+		await self.accept()
+
+	async def disconnect(self, close_code):
+		print('disconnect')
+		# Leave room group
+		await self.channel_layer.group_discard(
+			self.room_group_name,
+			self.channel_name
+		)
+
+	# Receive message from WebSocket
+	# noinspection PyMethodOverriding
+	async def receive(self, message_data):
+		print('receive')
+		text_data_json = json.loads(message_data, encoding='utf8')
+		message = text_data_json['message']
+		nickname = text_data_json['nickname']
+		datetime = text_data_json['datetime']
+
 		# Send message to room group
 		await self.channel_layer.group_send(
 			self.room_group_name,
@@ -54,24 +110,25 @@ class ChatConsumer(AsyncWebsocketConsumer):
 				'datetime': datetime,
 			}
 		)
-		print('after room_group_send')
 
 	# Receive message from room group
 	async def chat_message(self, event):
+		print('chat_message')
 		message = event['message']
 		nickname = event['nickname']
 		datetime = event['datetime']
 
-		print('before send')
+		text_data = json.dumps({
+			'message': message,
+			'nickname': nickname,
+			'datetime': datetime,
+		},
+			ensure_ascii=False,
+			encoding='utf8'
+		)
+		print(text_data)
+
 		# Send message to WebSocket
 		await self.send(
-			text_data=json.dumps(
-				{
-					'message': message,
-					'nickname': nickname,
-					'datetime': datetime,
-				}
-			)
+			text_data=text_data
 		)
-
-		print('after send')
