377b77cc6981af02944526e6e9df9275aadc1d36
Evgeny Timofeyev <nawasington@ya.ru>
2019-02-13 17:51:02 +0300

    trying to async consumer

--- blog/consumers.py
+++ blog/consumers.py
@@ -12,25 +12,25 @@ class ChatConsumer(WebsocketConsumer):
 		self.room_name = self.scope['url_route']['kwargs']['room_name']
 		self.room_group_name = 'chat_%s' % self.room_name
 
-	def connect(self):
+	async def connect(self):
 		# Join room group
-		async_to_sync(self.channel_layer.group_add)(
+		await self.channel_layer.group_add(
 			self.room_group_name,
 			self.channel_name
 		)
 
 		self.accept()
 
-	def disconnect(self, close_code):
+	async def disconnect(self, close_code):
 		# Leave room group
-		async_to_sync(self.channel_layer.group_discard)(
+		await self.channel_layer.group_discard(
 			self.room_group_name,
 			self.channel_name
 		)
 
 	# Receive message from WebSocket
 	# noinspection PyMethodOverriding
-	def receive(self, text_data):
+	async def receive(self, text_data):
 		text_data_json = json.loads(text_data, encoding='utf8')
 		message = text_data_json['message']
 		nickname = text_data_json['nickname']
@@ -46,7 +46,7 @@ class ChatConsumer(WebsocketConsumer):
 		comment.save()
 
 		# Send message to room group
-		async_to_sync(self.channel_layer.group_send)(
+		await self.channel_layer.group_send(
 			self.room_group_name,
 			{
 				'type': 'chat_message',
