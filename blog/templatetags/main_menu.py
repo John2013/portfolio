554081f44dea3229fc47a7d50da5cabd754c957d
Evgeny Timofeyev <nawasington@ya.ru>
2019-02-15 18:36:45 +0300

    menu auto active item

--- blog/templatetags/main_menu.py
+++ blog/templatetags/main_menu.py
@@ -6,7 +6,7 @@ register = template.Library()
 
 @register.inclusion_tag('main_menu.html')
 def main_menu(request):
-	current_url = resolve(request.path_info)
+	current_url = resolve(request.path_info).view_name
 	items = [
 		('Блог', reverse('blog:index')),
 		('Портфолио', reverse('portfolio:index'))
@@ -14,7 +14,7 @@ def main_menu(request):
 
 	def menu_item_append_active(item):
 		title, url = item
-		return title, url, resolve(url) == current_url
+		return title, url, resolve(url).view_name == current_url
 
 	items = map(
 		menu_item_append_active,
