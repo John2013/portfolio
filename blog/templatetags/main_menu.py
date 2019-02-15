e3193ac0044aef3d7e5337f815fb9c068ba769c9
Evgeny Timofeyev <nawasington@ya.ru>
2019-02-15 18:00:51 +0300

    menu items from function

--- blog/templatetags/main_menu.py
+++ blog/templatetags/main_menu.py
@@ -1,9 +1,13 @@
 from django import template
+from django.urls import reverse
 
 register = template.Library()
 
 
 @register.inclusion_tag('main_menu.html')
 def main_menu():
-	items = []
+	items = [
+		('Блог', reverse('blog:index'), True),
+		('Портфолио', reverse('portfolio:index'), False)
+	]
 	return {'items': items}
