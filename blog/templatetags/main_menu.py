820898670afe99f5f277b4f501c006c531897149
Evgeny Timofeyev <nawasington@ya.ru>
2019-02-15 18:15:26 +0300

    menu auto active item

--- blog/templatetags/main_menu.py
+++ blog/templatetags/main_menu.py
@@ -1,13 +1,18 @@
 from django import template
-from django.urls import reverse
+from django.urls import reverse, resolve
 
 register = template.Library()
 
 
 @register.inclusion_tag('main_menu.html')
-def main_menu():
+def main_menu(request):
+	current_url = resolve(request.path_info).url_name
 	items = [
-		('Блог', reverse('blog:index'), True),
-		('Портфолио', reverse('portfolio:index'), False)
+		('Блог', reverse('blog:index')),
+		('Портфолио', reverse('portfolio:index'))
 	]
+	items = map(
+		lambda title, url: (title, url, resolve(url) == current_url),
+		items
+	)
 	return {'items': items}
