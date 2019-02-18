56502255575c8948dc1271fb339f4d947789b7c5
Evgeny Timofeyev <nawasington@ya.ru>
2019-02-18 19:03:16 +0300

    setting media on aws s3

--- mysite/settings.py
+++ mysite/settings.py
@@ -143,7 +143,13 @@ STATIC_ROOT = os.path.join(
 	'static_files'
 )
 
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "")
+AWS_STORAGE_BUCKET_NAME = os.environ.get("S3_BUCKET_NAME", "")
+AWS_QUERYSTRING_AUTH = False
+AWS_S3_CUSTOM_DOMAIN = os.environ.get("S3_CUSTOM_DOMAIN", "")
+MEDIA_ROOT = os.environ.get("MEDIA_URL", "")
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join('/tmp', 'media')
 
 django_heroku.settings(locals())
