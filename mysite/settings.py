46a36e8f727a00bd874f634c817757c2b1716a5c
Evgeny Timofeyev <nawasington@ya.ru>
2019-02-18 16:40:44 +0300

    setting media root out of the app

--- mysite/settings.py
+++ mysite/settings.py
@@ -144,6 +144,6 @@ STATIC_ROOT = os.path.join(
 )
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join('/tmp', 'media')
 
 django_heroku.settings(locals())
