f0ce60ac0a3db0bcea72d5291db077568485e341
Evgeny Timofeyev <nawasington@ya.ru>
2019-02-18 14:39:59 +0300

    work model

--- portfolio/apps.py
+++ portfolio/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class PortfolioConfig(AppConfig):
     name = 'portfolio'
+
+    class Meta:
+        verbose_name = 'Портфолио'
--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+from blog.models import Tag
+
+
+class Work(models.Model):
+	name = models.CharField(
+		'Название', max_length=255, null=False, default='', unique=True
+	)
+	url = models.URLField()
+	short_description = models.CharField('Краткое описание', max_length=63)
+	description = models.TextField('Описание')
+	image = models.ImageField('Изображение')
+	slug = models.SlugField(null=False, default='', unique=True, blank=True)
+
+	tags = models.ManyToManyField(Tag, verbose_name='Теги')
+
+	created_at = models.DateTimeField('Создано', auto_now_add=True, blank=True)
+	updated_at = models.DateTimeField('Изменено', auto_now=True, blank=True)
+
+	def __str__(self):
+		return self.name
+
+	class Meta:
+		verbose_name = "Работа"
+		verbose_name_plural = "Работы"
+
+	def _get_unique_slug(self):
+		slug_max_size = self.slug.max_length
+		slug = slugify(self.name, allow_unicode=True)[:slug_max_size]
+		unique_slug = slug
+		num = 1
+
+		while Work.objects.filter(slug=unique_slug).exists():
+			# обрезаем размер слага, чтобы он влез в поле
+			postfix_size = len(str(num)) + 1
+			unique_slug = '{}-{}'.format(
+				slug[:(slug_max_size - postfix_size)], num
+			)
+			num += 1
+
+		return unique_slug
+
+	def save(self, *args, **kwargs):
+		self.slug = self._get_unique_slug()
+
+		super().save(*args, **kwargs)
+
