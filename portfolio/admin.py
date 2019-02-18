dba83732788173d1b552bb284cc7a0a255c96215
Evgeny Timofeyev <nawasington@ya.ru>
2019-02-18 15:19:58 +0300

    portfolio admin settings

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
 
-# Register your models here.
+from portfolio.models import Work
+
+
+class WorkAdmin(admin.ModelAdmin):
+	fieldsets = [
+		(
+			None,
+			{'fields': [
+				'name',
+				'image',
+				'url',
+				'url_source',
+				'slug'
+			]}
+		),
+		(
+			'Описание',
+			{
+				'fields': ['short_description', 'description'],
+				'classes': ['collapse']
+			}
+		),
+		(
+			'Теги',
+			{'fields': ['tags']}
+		),
+		(
+			'Даты',
+			{'fields': ['created_at', 'updated_at'], 'classes': ['collapse']}
+		),
+	]
+	filter_horizontal = ('tags',)
+	list_display = ('name', 'created_at', 'updated_at')
+	list_filter = ['tags', 'created_at', 'updated_at']
+	search_fields = ['name', 'short_description', 'description']
+
+
+admin.site.register(Work, WorkAdmin)
