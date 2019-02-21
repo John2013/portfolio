b619c4705cd373ec73a239d7e7b85ec44fc760d2
Evgeny Timofeyev <nawasington@ya.ru>
2019-02-21 16:07:58 +0300

    setup yandexcloud

--- mysite/settings.py
+++ mysite/settings.py
@@ -155,7 +155,7 @@ AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "")
 # AWS_SESSION_TOKEN = os.environ.get("AWS_SESSION_TOKEN", "")
 
 AWS_S3_BUCKET_NAME = os.environ.get("S3_BUCKET_NAME", "")
-AWS_S3_ENDPOINT_URL = "storage.yandexcloud.net"
+AWS_S3_ENDPOINT_URL = "https://storage.yandexcloud.net"
 # AWS_STORAGE_BUCKET_NAME = os.environ.get("S3_BUCKET_NAME", "")
 # AWS_QUERYSTRING_AUTH = False
 # AWS_S3_CUSTOM_DOMAIN = os.environ.get("S3_CUSTOM_DOMAIN", "")
